3b9acf9a7f9fa1244a3b749d99d0e31d24131c76
horchakova24-beep <horchakova24@gmail.com>
2026-07-14 04:15:11 +0200

    Update generator.py

--- generator.py
+++ generator.py
@@ -57,8 +57,7 @@ def generate_words():
     """
 
     # Изменили v1beta на v1 для совместимости с моделью 2.5
-    url = f"[https://generativelanguage.googleapis.com/v1/models/gemini-2.5-flash:generateContent?key=](https://generativelanguage.googleapis.com/v1/models/gemini-2.5-flash:generateContent?key=){api_key}"
-
+url = f"https://generativelanguage.googleapis.com/v1beta/models/gemini-flash-latest:generateContent?key={api_key}"
     headers = {"Content-Type": "application/json"}
     data = {
         "contents": [{
